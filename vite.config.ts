d02d809a7af65c84fecb7eeb38b828da8e42febe
Gergert Ilya <gergert.ilya@mail.ru>
2023-04-14 21:30:05 +0300

    feat: add dts vite plugin and build lib options

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   build: {
     lib: {
-      entry: './lib/main.ts',
-      name: 'zustand-action',
-      fileName: 'zustand-action',
+      entry: './lib/index.ts',
+      name: 'zustand-toolkit',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom', 'zustand', 'immer'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'react',
+          'react-dom': 'ReactDOM',
+          zustand: 'zustand',
+          immer: 'immer',
+        },
+      },
     },
   },
 });
